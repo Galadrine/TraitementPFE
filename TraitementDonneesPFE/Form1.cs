e5224c376ef41f934cd97292ba8f0d877f7c24ab
Alexandre Barret <alex.barret92@gmail.com>
2016-12-13 14:26:49 +0100

    Create

---
+++ TraitementDonneesPFE/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TraitementDonneesPFE
+{
+    public partial class PFE_StabilisationCockpit : Form
+    {
+        public PFE_StabilisationCockpit()
+        {
+            InitializeComponent();
+        }
+    }
+}
